f3d2b9ff0a25887e2514496f04eb03bcf10f888c
TejaswiChV <chvtejaswi@gmail.com>
2024-04-07 12:05:04 +0530

    including bootstrap.mim.js and header footer

---
+++ ui-drama/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer" >
+             <div style={{align:"center",marginTop:"10px"}}>All rights reserved</div>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ ui-drama/src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const HeaderComponent = () => {
+  return (
+<>
+<nav className="navbar navbar-dark bg-dark">
+    <div className="navbar-brand" to="/" style={{marginLeft:"30px"}}>DramaLand</div>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  
+  </button>
+</nav>
+</>
+  )
+}
+
+export default HeaderComponent
